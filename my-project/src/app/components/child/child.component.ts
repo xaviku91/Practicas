314d2a28319cba112175b02fc5b44e09d4570566
Xaviku <xquesadas@uoc.edu>
2025-03-25 22:42:01 +0100

    Angular v2

---
+++ my-project/src/app/components/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input()
+  message: string = '';
+
+  @Input()
+  person: any
+}
