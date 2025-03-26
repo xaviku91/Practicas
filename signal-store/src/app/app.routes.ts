73f56316ebaf5ff4ccff0021774fd76c136ea6a6
xaviku91 <xquesadas@uoc.edu>
2025-03-26 13:57:44 +0100

    Curso angular 18

---
+++ signal-store/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: 'products',
+        loadChildren: () => import('./productos/features/product-shell/product-route'),
+    },
+    {
+        path: '**',
+        redirectTo: 'products',
+    }
+];
---
+++ signal-store/src/app/products/features/product-shell/product-route.ts
@@ -0,0 +1,5 @@
+import { Routes } from "@angular/router";
+
+export default [
+    { path: '', loadComponent: () => import('../product-list/product-list.component') },
+] as Routes;
